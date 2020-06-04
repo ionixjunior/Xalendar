6899a86d8da45109331699a6b065d70e40ca7b48
Ione Souza Junior <junior@ionixjunior.com.br>
2020-06-04 20:53:37 -0300

    Create day entitiy and implement IsToday property

---
+++ src/Xalendar.Api/Models/Day.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Xalendar.Api.Models
+{
+    public class Day
+    {
+        private readonly DateTime _dateTime;
+
+        public Day(DateTime dateTime)
+        {
+            _dateTime = dateTime;
+        }
+
+        public bool IsToday => DateTime.Now.Day == _dateTime.Day;
+    }
+}
