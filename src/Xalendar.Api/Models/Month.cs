32b7528d3621b391a11dbed8678a7e7308a19de7
Ione Souza Junior <junior@ionixjunior.com.br>
2020-06-04 21:40:48 -0300

    Add support to get days of month

---
+++ src/Xalendar.Api/Models/Month.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Xalendar.Api.Models
+{
+    public class Month
+    {
+        private readonly DateTime _dateTime;
+        private readonly IReadOnlyList<Day> _days;
+
+        public Month(DateTime dateTime)
+        {
+            _dateTime = dateTime;
+            _days = GenerateDaysOfMonth();
+        }
+
+        private List<Day> GenerateDaysOfMonth()
+        {
+            return Enumerable
+                .Range(1, DateTime.DaysInMonth(_dateTime.Year, _dateTime.Month))
+                .Select(dayValue => new DateTime(_dateTime.Year, _dateTime.Month, dayValue))
+                .Select(dateTime => new Day(dateTime))
+                .ToList();
+        }
+
+        public IReadOnlyList<Day> GetDaysOfMonth()
+        {
+            return _days;
+        }
+    }
+}
