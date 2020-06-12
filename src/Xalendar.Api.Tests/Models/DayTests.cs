b606cb66ff6f68efbc453b526eecffe502554084
Ione Souza Junior <junior@ionixjunior.com.br>
2020-06-11 23:47:46 -0300

    Add test for equivalent parameter

--- src/Xalendar.Api.Tests/Models/DayTests.cs
+++ src/Xalendar.Api.Tests/Models/DayTests.cs
@@ -73,5 +73,27 @@ namespace Xalendar.Api.Tests.Models
             
             Assert.IsFalse(comparison);
         }
+
+        [Test]
+        public void DayShouldBeEquals()
+        {
+            var dayOne = new Day(DateTime.Now);
+            var dayTwo = new Day(DateTime.Now);
+
+            var comparison = dayOne.Equals(dayTwo);
+
+            Assert.IsTrue(comparison);
+        }
+        
+        [Test]
+        public void DayShouldNotBeEquals()
+        {
+            var dayOne = new Day(DateTime.Now);
+            var dayTwo = new Day(DateTime.Now.AddDays(1));
+
+            var comparison = dayOne.Equals(dayTwo);
+
+            Assert.IsFalse(comparison);
+        }
     }
 }
