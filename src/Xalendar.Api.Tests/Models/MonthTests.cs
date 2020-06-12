64076bf0e8b5b9492c2b7f34b860b382ad6b1430
Ione Souza Junior <junior@ionixjunior.com.br>
2020-06-11 23:33:17 -0300

    Test when selected day changed

--- src/Xalendar.Api.Tests/Models/MonthTests.cs
+++ src/Xalendar.Api.Tests/Models/MonthTests.cs
@@ -58,7 +58,17 @@ namespace Xalendar.Api.Tests.Models
         [Test]
         public void SelectedDayShouldBeChanged()
         {
+            var dateTime = new DateTime(2020, 2, 1);
+            var month = new Month(dateTime);
+            var isSelected = true;
+            var day = new Day(dateTime, isSelected);
+            month.SelectDay(day);
+            var newDaySelected = new Day(dateTime.AddDays(10), isSelected);
+            month.SelectDay(newDaySelected);
+
+            var selectedDay = month.GetSelectedDay();
             
+            Assert.AreEqual(newDaySelected, selectedDay);
         }
     }
 }
