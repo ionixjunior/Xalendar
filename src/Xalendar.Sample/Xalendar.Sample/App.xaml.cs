c5f088c3231d6693b3e48cd40c6af431aae69152
Ione Souza Junior <junior@ionixjunior.com.br>
2020-07-16 22:58:51 -0300

    Add navigation page to main page

--- src/Xalendar.Sample/Xalendar.Sample/App.xaml.cs
+++ src/Xalendar.Sample/Xalendar.Sample/App.xaml.cs
@@ -12,7 +12,7 @@ namespace Xalendar.Sample
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
