e2da6eb0b92f8dea7c55d6e74c22361f67cfca47
Ione Souza Junior <junior@ionixjunior.com.br>
2020-07-16 23:06:30 -0300

    Add CalendarView control

---
+++ src/Xalendar.View/Controls/CalendarView.xaml.cs
@@ -0,0 +1,15 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xalendar.View.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalendarView : ContentView
+    {
+        public CalendarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
