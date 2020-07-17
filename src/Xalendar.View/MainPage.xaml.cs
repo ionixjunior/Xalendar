c49d8ac891570ba54cf2811b6f5de664d6032f06
Ione Souza Junior <junior@ionixjunior.com.br>
2020-07-16 21:06:47 -0300

    Create Xalendar.View project

---
+++ src/Xalendar.View/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Xalendar.View
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
