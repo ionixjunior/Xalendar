cfad12c32d25a39a514bb3bb7b72dacb71d21049
Ione Souza Junior <junior@ionixjunior.com.br>
2020-07-23 22:00:16 -0300

    Add support to discard previous and next days of another months

--- src/Xalendar.Api/Models/MonthContainer.cs
+++ src/Xalendar.Api/Models/MonthContainer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 
 namespace Xalendar.Api.Models
 {
@@ -9,12 +10,35 @@ namespace Xalendar.Api.Models
         [EditorBrowsable(EditorBrowsableState.Never)]
         public Month _month;
         
-        public IReadOnlyList<Day> Days { get; }
+        public IReadOnlyList<Day?> Days { get; }
         
         public MonthContainer(DateTime dateTime)
         {
             _month = new Month(dateTime);
-            Days = _month.Days;
+            
+            var daysOfContainer = new List<Day?>();
+            GetDaysToDiscardAtStartOfMonth(daysOfContainer);
+            daysOfContainer.AddRange(_month.Days);
+            GetDaysToDiscardAtEndOfMonth(daysOfContainer);
+            Days = daysOfContainer;
+        }
+
+        private void GetDaysToDiscardAtStartOfMonth(List<Day?> daysOfContainer)
+        {
+            var firstDay = _month.Days.First();
+            var numberOfDaysToDiscard = (int) firstDay.DateTime.DayOfWeek;
+            
+            for (var index = 0; index < numberOfDaysToDiscard; index++)
+                daysOfContainer.Add(default(Day));
+        }
+
+        private void GetDaysToDiscardAtEndOfMonth(List<Day?> daysOfContainer)
+        {
+            var lastDay = _month.Days.Last();
+            var numberOfDaysToDiscard = 6 - (int) lastDay.DateTime.DayOfWeek;
+            
+            for (var index = 0; index < numberOfDaysToDiscard; index++)
+                daysOfContainer.Add(default(Day));
         }
     }
 }
