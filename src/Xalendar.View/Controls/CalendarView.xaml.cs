d1cf6d779787535860741fd177814313dfa3a1ef
Ione Souza Junior <junior@ionixjunior.com.br>
2020-08-20 21:50:33 -0300

    Move list of days to code behind

--- src/Xalendar.View/Controls/CalendarView.xaml.cs
+++ src/Xalendar.View/Controls/CalendarView.xaml.cs
@@ -1,3 +1,5 @@
+using System;
+using Xalendar.Api.Models;
 using Xalendar.View.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -7,9 +9,14 @@ namespace Xalendar.View.Controls
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CalendarView : ContentView
     {
+        private readonly MonthContainer _monthContainer;
+        
         public CalendarView()
         {
             InitializeComponent();
+            
+            _monthContainer = new MonthContainer(DateTime.Today);
+            BindableLayout.SetItemsSource(CalendarDaysContainer, _monthContainer.Days);
 
             BindingContext = new CalendarViewModel();
         }
