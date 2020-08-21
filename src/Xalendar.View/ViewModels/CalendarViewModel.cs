e7aea8c5b8d298e9d893632c5b6eb870608b7b3d
Ione Souza Junior <junior@ionixjunior.com.br>
2020-08-20 21:33:04 -0300

    Create commands to navigate between months

--- src/Xalendar.View/ViewModels/CalendarViewModel.cs
+++ src/Xalendar.View/ViewModels/CalendarViewModel.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Windows.Input;
 using Xalendar.Api.Extensions;
 using Xalendar.Api.Models;
+using Xamarin.Forms;
 
 namespace Xalendar.View.ViewModels
 {
@@ -12,6 +14,8 @@ namespace Xalendar.View.ViewModels
         public IReadOnlyList<Day?> Days { get; }
         public IReadOnlyList<string> DaysOfWeek { get; }
         public string MonthName { get; }
+        public ICommand NavigateToPreviousMonthCommand { get; }
+        public ICommand NavigateToNextMonthCommand { get; }
         
         public CalendarViewModel()
         {
@@ -19,6 +23,19 @@ namespace Xalendar.View.ViewModels
             Days = _monthContainer.Days;
             DaysOfWeek = _monthContainer.DaysOfWeek;
             MonthName = _monthContainer.GetName();
+
+            NavigateToPreviousMonthCommand = new Command(NavigateToPreviousMonth);
+            NavigateToNextMonthCommand = new Command(NavigateToNextMonth);
+        }
+
+        private void NavigateToPreviousMonth()
+        {
+            System.Diagnostics.Debug.WriteLine("navegar para mês anterior");
+        }
+        
+        private void NavigateToNextMonth()
+        {
+            System.Diagnostics.Debug.WriteLine("navegar para próximo mês");
         }
     }
 }
