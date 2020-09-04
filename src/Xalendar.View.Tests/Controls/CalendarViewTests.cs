8d008329fa96f48e32e808dee91497a8e57602bb
Ione Souza Junior <junior@ionixjunior.com.br>
2020-09-03 23:32:23 -0300

    Add test for previous button click

---
+++ src/Xalendar.View.Tests/Controls/CalendarViewTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using Xalendar.View.Controls;
+using Xamarin.Forms;
+
+namespace Xalendar.View.Tests.Controls
+{
+    [TestFixture]
+    public class CalendarViewTests
+    {
+        private CalendarView _calendarView;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            Xamarin.Forms.Mocks.MockForms.Init();
+            _calendarView = new CalendarView();
+        }
+
+        [Test]
+        public void ShouldNavigateToPreviousMonthWhenPreviousButtonClick()
+        {
+            var button = _calendarView.FindByName<Button>("PreviousButton");
+            var monthName = _calendarView.FindByName<Label>("MonthName");
+            
+            button.SendClicked();
+
+            var previousMonth = DateTime.Today.AddMonths(-1);
+            Assert.AreEqual(previousMonth.ToString("MMMM yyyy"), monthName.Text);
+        }
+    }
+}
