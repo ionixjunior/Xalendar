9a50e3802489556f0075862e77df03874908f3be
Ione Souza Junior <junior@ionixjunior.com.br>
2020-09-10 23:12:30 -0300

    Add sample events in MainPage

--- src/Xalendar.Sample/Xalendar.Sample/MainPage.xaml.cs
+++ src/Xalendar.Sample/Xalendar.Sample/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xalendar.Api.Models;
 using Xamarin.Forms;
 
 namespace Xalendar.Sample
@@ -12,6 +13,16 @@ namespace Xalendar.Sample
         public MainPage()
         {
             InitializeComponent();
+
+            var events = new List<Event>();
+
+            for (var index = 1; index <= 10; index++)
+            {
+                var eventDate = new DateTime(2020, 9, index);
+                events.Add(new Event(index, "Nome evento", eventDate, eventDate, false));
+            }
+            
+            Calendar.Events = events;
         }
     }
 }
