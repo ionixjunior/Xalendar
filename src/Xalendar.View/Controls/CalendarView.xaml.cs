11eeef0d6278f28f2ef21583654ebe4032449d9e
Ione Souza Junior <junior@ionixjunior.com.br>
2020-11-05 21:33:50 -0300

    Remove call of MonthChanged in LayoutChanged event

--- src/Xalendar.View/Controls/CalendarView.xaml.cs
+++ src/Xalendar.View/Controls/CalendarView.xaml.cs
@@ -142,14 +142,7 @@ namespace Xalendar.View.Controls
                 
                 BindableLayout.SetItemsSource(CalendarDaysOfWeekContainer, _monthContainer.DaysOfWeek);
                 MonthName.Text = _monthContainer.GetName();
-                
-                this.LayoutChanged += OnLayoutChanged;
-
-                void OnLayoutChanged(object _, EventArgs __)
-                {
-                    this.LayoutChanged -= OnLayoutChanged;
-                    MonthChanged?.Invoke(new MonthRange(_monthContainer.FirstDay, _monthContainer.LastDay));
-                }
+                MonthChanged?.Invoke(new MonthRange(_monthContainer.FirstDay, _monthContainer.LastDay));
             }
         }
 
