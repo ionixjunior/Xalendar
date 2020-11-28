65b3abf703e365982ebf2f6994beac8f312d619c
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2020-11-28 18:55:03 -0300

    Adicionando referencia de assembly para schema

---
+++ src/Xalendar.View/Properties/PreserveXalendarViewAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Xalendar.View
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public sealed class PreserveXalendarViewAttribute : Attribute
+    {
+    }
+}
