fb2953ab1edfbcb434a6b77c5817d17895cc25d5
Ione Souza Junior <junior@ionixjunior.com.br>
2021-03-02 18:49:54 -0300

    Create class DayOfWeek2CaractersFormat

---
+++ src/Xalendar/Api/Formatters/DayOfWeek2CaractersFormat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xalendar.Api.Interfaces;
+
+namespace Xalendar.Api.Formatters
+{
+    public class DayOfWeek2CaractersFormat : IDayOfWeekFormatter
+    {
+        public string Format(DayOfWeek dayOfWeek)
+        {
+            return CultureInfo
+                .CurrentCulture
+                .DateTimeFormat
+                .GetDayName(dayOfWeek)
+                .Substring(0, 2)
+                .ToLower();
+        }
+    }
+}
