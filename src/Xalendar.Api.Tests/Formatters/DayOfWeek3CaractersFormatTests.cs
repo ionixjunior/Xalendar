736a423a336c381845194e428893f8b9d5b2485f
Ione Souza Junior <junior@ionixjunior.com.br>
2021-03-02 18:57:19 -0300

    Create class DayOfWeek3CaractersFormat

---
+++ src/Xalendar.Api.Tests/Formatters/DayOfWeek3CaractersFormatTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+using Xalendar.Api.Formatters;
+
+namespace Xalendar.Api.Tests.Formatters
+{
+    [TestFixture]
+    public class DayOfWeek3CaractersFormatTests
+    {
+        [Test]
+        [TestCaseSource(nameof(ValuesForTheDaysOfWeekInSpecificLanguagesTests))]
+        public void MonthContainerShouldContainsTheDaysOfWeekInSpecificLanguages(string language, DayOfWeek dayOfWeek, string dayOfWeekName)
+        {
+            CultureInfo.CurrentCulture = new CultureInfo(language);
+            var formatter = new DayOfWeek3CaractersFormat();
+
+            var result = formatter.Format(dayOfWeek);
+
+            Assert.AreEqual(dayOfWeekName, result);
+        }
+
+        private static object[] ValuesForTheDaysOfWeekInSpecificLanguagesTests =
+        {
+            new object[] { "pt-BR", DayOfWeek.Sunday, "dom" },
+            new object[] { "en-US", DayOfWeek.Sunday, "sun" },
+            new object[] { "fr-FR", DayOfWeek.Sunday, "dim" }
+        };
+    }
+}
---
+++ src/Xalendar/Api/Formatters/DayOfWeek3CaractersFormat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xalendar.Api.Interfaces;
+
+namespace Xalendar.Api.Formatters
+{
+    public class DayOfWeek3CaractersFormat : IDayOfWeekFormatter
+    {
+        public string Format(DayOfWeek dayOfWeek)
+        {
+            return CultureInfo
+                .CurrentCulture
+                .DateTimeFormat
+                .GetDayName(dayOfWeek)
+                .Substring(0, 3)
+                .ToLower();
+        }
+    }
+}
