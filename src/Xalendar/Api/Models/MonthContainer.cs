1972c9aec819c8df394f0e6b16c6f9cada3fc383
Ione Souza Junior <junior@ionixjunior.com.br>
2021-03-01 22:35:07 -0300

    Add support to preview the next and previous days of month

--- src/Xalendar/Api/Models/MonthContainer.cs
+++ src/Xalendar/Api/Models/MonthContainer.cs
@@ -9,7 +9,9 @@ namespace Xalendar.Api.Models
 {
     public class MonthContainer
     {
+        internal Month _previousMonth;
         internal Month _currentMonth;
+        internal Month _nextMonth;
 
         private IReadOnlyList<Day?>? _days;
         public IReadOnlyList<Day?> Days => _days ??= GetDaysOfContainer();
@@ -23,7 +25,9 @@ namespace Xalendar.Api.Models
         
         public MonthContainer(DateTime dateTime, DayOfWeek firstDayOfWeek = DayOfWeek.Sunday)
         {
+            _previousMonth = new Month(dateTime.AddMonths(-1));
             _currentMonth = new Month(dateTime);
+            _nextMonth = new Month(dateTime.AddMonths(1));
             _firstDayOfWeek = firstDayOfWeek;
 
             DaysOfWeek = GenerateDaysOfWeek(firstDayOfWeek)
@@ -53,12 +57,44 @@ namespace Xalendar.Api.Models
         private IReadOnlyList<Day?> GetDaysOfContainer()
         {
             var daysOfContainer = new List<Day?>();
-            this.GetDaysToDiscardAtStartOfMonth(daysOfContainer);
+            //this.GetDaysToDiscardAtStartOfMonth(daysOfContainer);
+            FillDaysOfPreviousMonth(daysOfContainer);
             daysOfContainer.AddRange(_currentMonth.Days);
-            this.GetDaysToDiscardAtEndOfMonth(daysOfContainer);
+            //this.GetDaysToDiscardAtEndOfMonth(daysOfContainer);
+            FillDaysOfNextMonth(daysOfContainer);
             return daysOfContainer;
         }
-        
+
+        private void FillDaysOfPreviousMonth(List<Day?> daysOfContainer)
+        {
+            var firstDay = _currentMonth.Days.First();
+            var differenceOfDays = ((int)_firstDayOfWeek - (int)firstDay!.DateTime.DayOfWeek);
+            var numberOfDaysToShow = differenceOfDays <= 0 ? Math.Abs(differenceOfDays) : 7 - differenceOfDays;
+
+            var days = _previousMonth
+                .Days
+                .Skip(_previousMonth.Days.Count - numberOfDaysToShow)
+                .Take(numberOfDaysToShow);
+
+            foreach (var day in days)
+                daysOfContainer.Add(day);
+        }
+
+        private void FillDaysOfNextMonth(List<Day?> daysOfContainer)
+        {
+            if (daysOfContainer.Count < 42)
+            {
+                var numberOfDaysToShow = 42 - daysOfContainer.Count;
+
+                var days = _nextMonth
+                    .Days
+                    .Take(numberOfDaysToShow);
+
+                foreach (var day in days)
+                    daysOfContainer.Add(day);
+            }
+        }
+
         private void GetDaysToDiscardAtStartOfMonth(List<Day?> daysOfContainer)
         {
             var firstDay = _currentMonth.Days.First();
@@ -79,14 +115,18 @@ namespace Xalendar.Api.Models
         public void Next()
         {
             var nextDateTime = _currentMonth.MonthDateTime.AddMonths(1);
+            _previousMonth = new Month(nextDateTime.AddMonths(-1));
             _currentMonth = new Month(nextDateTime);
+            _nextMonth = new Month(nextDateTime.AddMonths(1));
             _days = null;
         }
 
         public void Previous()
         {
             var previousDateTime = _currentMonth.MonthDateTime.AddMonths(-1);
+            _previousMonth = new Month(previousDateTime.AddMonths(-1));
             _currentMonth = new Month(previousDateTime);
+            _nextMonth = new Month(previousDateTime.AddMonths(1));
             _days = null;
         }
     }
