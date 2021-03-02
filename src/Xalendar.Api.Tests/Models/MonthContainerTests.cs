e36ddf7da2901d842793fcaa697976df114e04b2
Ione Souza Junior <junior@ionixjunior.com.br>
2021-03-01 23:15:56 -0300

    Fix tests

--- src/Xalendar.Api.Tests/Models/MonthContainerTests.cs
+++ src/Xalendar.Api.Tests/Models/MonthContainerTests.cs
@@ -231,48 +231,25 @@ namespace Xalendar.Api.Tests.Models
 
         [Test]
         [TestCaseSource(nameof(ValuesForDaysOfMonthShouldStartBasedOnFirstDayOfWeek))]
-        public void DaysOfMonthShouldStartBasedOnFirstDayOfWeek(DateTime dateTime, DayOfWeek firstDayOfWeek, List<Day?> expectedDays)
+        public void DaysOfMonthShouldStartBasedOnFirstDayOfWeek(DateTime dateTime, DayOfWeek firstDayOfWeek, int index)
         {
             var monthContainer = new MonthContainer(dateTime, firstDayOfWeek);
 
             var days = monthContainer.Days;
-            
-            CollectionAssert.AreEqual(expectedDays, days);
+
+            var firstDayOfCurrentMonth = days.First(day => day is { } && day.DateTime.Day == 1);
+            Assert.AreEqual(firstDayOfCurrentMonth, days.ElementAt(index));
         }
 
         private static object[] ValuesForDaysOfMonthShouldStartBasedOnFirstDayOfWeek =
         {
-            new object[] { new DateTime(2020, 9, 1), DayOfWeek.Sunday, GenerateDaysOfSeptember2020(2, 3) },
-            new object[] { new DateTime(2020, 9, 1), DayOfWeek.Monday, GenerateDaysOfSeptember2020(1, 4) },
-            new object[] { new DateTime(2020, 9, 1), DayOfWeek.Tuesday, GenerateDaysOfSeptember2020(0, 5) },
-            new object[] { new DateTime(2020, 9, 1), DayOfWeek.Wednesday, GenerateDaysOfSeptember2020(6, 6) },
-            new object[] { new DateTime(2020, 9, 1), DayOfWeek.Thursday, GenerateDaysOfSeptember2020(5, 7) },
-            new object[] { new DateTime(2020, 9, 1), DayOfWeek.Friday, GenerateDaysOfSeptember2020(4, 1) },
-            new object[] { new DateTime(2020, 9, 1), DayOfWeek.Saturday, GenerateDaysOfSeptember2020(3, 2) }
+            new object[] { new DateTime(2020, 9, 1), DayOfWeek.Sunday, 2},
+            new object[] { new DateTime(2020, 9, 1), DayOfWeek.Monday, 1},
+            new object[] { new DateTime(2020, 9, 1), DayOfWeek.Tuesday, 0},
+            new object[] { new DateTime(2020, 9, 1), DayOfWeek.Wednesday, 6},
+            new object[] { new DateTime(2020, 9, 1), DayOfWeek.Thursday, 5},
+            new object[] { new DateTime(2020, 9, 1), DayOfWeek.Friday, 4},
+            new object[] { new DateTime(2020, 9, 1), DayOfWeek.Saturday, 3}
         };
-
-        private static List<Day?> GenerateDaysOfSeptember2020(int daysToDiscardAtStart, int daysToDiscardAtEnd)
-        {
-            var days = new List<Day?>();
-            
-            for (var index = 0; index < daysToDiscardAtStart; index++)
-                days.Add(default(Day));
-
-            var dateTime = new DateTime(2020, 9, 1);
-            for (var index = 1; index <= 30; index++)
-            {
-                days.Add(new Day(dateTime));
-                dateTime = dateTime.AddDays(1);
-            }
-            
-            for (var index = 0; index < daysToDiscardAtEnd; index++)
-                days.Add(default(Day));
-            
-            if (days.Count < 42)
-                for (var index = days.Count; index < 42; index++)
-                    days.Add(default(Day));
-            
-            return days;
-        }
     }
 }
