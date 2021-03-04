87a492236a8f707fc0c5260390e5e24e9adb5e7c
Ione Souza Junior <junior@ionixjunior.com.br>
2021-03-04 16:50:49 -0300

    Add test to DayOfWeek1CaractersFormat

---
+++ src/Xalendar.Tests/Api/Formatters/DayOfWeek1CaractersFormatTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+using Xalendar.Api.Formatters;
+
+namespace Xalendar.Tests.Api.Formatters
+{
+    [TestFixture]
+    public class DayOfWeek1CaractersFormatTests
+    {
+        [Test]
+        [TestCaseSource(nameof(ValuesForTheDaysOfWeekInSpecificLanguagesTests))]
+        public void MonthContainerShouldContainsTheDaysOfWeekInSpecificLanguages(string language, DayOfWeek dayOfWeek, string dayOfWeekName)
+        {
+            CultureInfo.CurrentCulture = new CultureInfo(language);
+            var formatter = new DayOfWeek1CaractersFormat();
+
+            var result = formatter.Format(dayOfWeek);
+
+            Assert.AreEqual(dayOfWeekName, result);
+        }
+
+        private static object[] ValuesForTheDaysOfWeekInSpecificLanguagesTests =
+        {
+            new object[] { "pt-BR", DayOfWeek.Sunday, "d" },
+            new object[] { "en-US", DayOfWeek.Sunday, "s" },
+            new object[] { "fr-FR", DayOfWeek.Sunday, "d" }
+        };
+    }
+}
