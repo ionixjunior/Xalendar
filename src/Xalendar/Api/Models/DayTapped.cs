bbe6926ef072d3b68a662504458fe595f2f25189
Ione Souza Junior <junior@ionixjunior.com.br>
2021-05-22 14:42:31 -0300

    Add DayTapped struct

---
+++ src/Xalendar/Api/Models/DayTapped.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Xalendar.Api.Enums;
+using Xalendar.Api.Interfaces;
+
+namespace Xalendar.Api.Models
+{
+    public struct DayTapped
+    {
+        public DateTime DateTime { get; }
+        public IEnumerable<ICalendarViewEvent> Events { get; }
+        public DayState State { get; }
+
+        public DayTapped(DateTime dateTime, IEnumerable<ICalendarViewEvent> events, DayState state)
+        {
+            DateTime = dateTime;
+            Events = events;
+            State = state;
+        }
+    }
+}
